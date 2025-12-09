fd9d9659e41c02a9ae188c079ff37b145b237501
korsar312 <69364209+korsar312@users.noreply.github.com>
2025-12-09 17:28:19 +0300

    Шаблон

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vitePluginSvgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/",
 	plugins: [
 		react({
 			jsxImportSource: "@emotion/react",
