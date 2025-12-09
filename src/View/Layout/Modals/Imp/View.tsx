4e178fc7e2e78862049a7caecef1995abc71d17f
korsar312 <69364209+korsar312@users.noreply.github.com>
2025-12-09 20:55:01 +0300

    Шаблон

--- src/View/Layout/Modals/Imp/View.tsx
+++ src/View/Layout/Modals/Imp/View.tsx
@@ -2,7 +2,6 @@ import type Model from "./Model.ts";
 import Style from "./Style.ts";
 import type { NFC } from "./../../../../Logic/Libs/Util/TypesUtils";
 import WidgetDebug from "../../../Components/5.Widget/WidgetDebug";
-import AtomButtonIcon from "../../../Components/1.Atoms/AtomButton/Variables/AtomButtonIcon";
 
 const View: NFC<typeof Model> = (props) => {
 	const { isShowDebug, toggleDebug } = props;
@@ -11,9 +10,7 @@ const View: NFC<typeof Model> = (props) => {
 		<>
 			<WidgetDebug isShow={isShowDebug} onClose={toggleDebug} />
 
-			<div css={Style.debugBtn}>
-				<AtomButtonIcon icon={"Bug"} click={toggleDebug} />
-			</div>
+			<div css={Style.debugBtn}></div>
 		</>
 	);
 };
